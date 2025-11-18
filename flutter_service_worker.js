d88eda7793c3983012daef0d82895771f7df1f93
Raphael Lima <https://github.com/raphaelolimpio/Api_dicionario_Python.git>
2025-11-18 13:46:53 -0300

    Built web
    Branch: main
    Commit: ffdf024268dfc3b49f253cb277f98e208f71f81b
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fabc6bbff5e5a0e5030e5a62facfc4ce
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5d9881d1099b052209a8af1ef64ecef7",
+"flutter_bootstrap.js": "19601fbb261d83045e2d83c5629a5115",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
